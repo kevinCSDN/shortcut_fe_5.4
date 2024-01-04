074a6ecb59b88b6f0f105a2744801f48f4c066de
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-01-04 18:34:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,15 +12,27 @@
 
 # Uncomment a feed source
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #删除feeds自带mosdns、v2ray-geodata
 rm -rf ./feeds/packages/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata
 
 
 
+
+
+
+
+
+
+#克隆passwall环境插件
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+
+#克隆的源码放在small文件夹
+mkdir package/small
+pushd package/small
 
 #克隆源码
+
 #passwall2
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
@@ -28,8 +40,4 @@ git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 
 
-
-
-# Add a feed source
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages-backup' >>feeds.conf.default
+popd
