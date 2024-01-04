e321f7786ffdda2e07d68e95f120ecd227d7f5cf
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2024-01-04 18:04:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,20 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#删除feeds自带mosdns、v2ray-geodata
+rm -rf ./feeds/packages/net/mosdns
 
+
+
+#克隆源码
+#passwall2
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+#mosdns
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+
+
+
+popd
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git sundaqiang https://github.com/sundaqiang/openwrt-packages-backup' >>feeds.conf.default
